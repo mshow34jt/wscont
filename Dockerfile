fdae359c5cfcd7323ee3472089aa4d8f6d65f17b
Jeremy Enos <jenos@ammo.localdomain>
2021-09-02 00:31:49 -0500

    Changes for rootless docker and singularity, work toward read-only

--- Dockerfile
+++ Dockerfile
@@ -20,7 +20,8 @@ RUN yum update -y && yum group install -y "Development Tools" && \
                    autogen \
                    git \
                    && pip3 install cython pandas mod_wsgi && \
-    yum clean all
+    yum clean all && \
+    mkdir /source # needed for Singularity recipe conversion compatibility
 
 WORKDIR /source
 
@@ -91,8 +92,27 @@ RUN ln -s /usr/local/lib64/python3.6/site-packages/mod_wsgi/server/mod_wsgi-py36
     ln -s /var/log/ovis_web_svcs /log && \
     rm -f /etc/httpd/logs && \
     ln -s /var/log/ovis_web_svcs /etc/httpd/logs && \
-    chown -R apache:apache /var/www/ovis_web_svcs && \
-    chmod -R g+rw /var/www/ovis_web_svcs && \
+    cp /custom/settings.py /var/www/ovis_web_svcs/sosgui/settings.py && \
+    #ln -s /custom/settings.py /var/www/ovis_web_svcs/sosgui/settings.py && \
+    touch /log/settings.log && \
+    python3 manage.py migrate && \
+    python3 manage.py migrate --run-syncdb && \
+    echo "from sosdb_auth.models import SosdbUser; SosdbUser.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python3 manage.py shell && \
+    python3 manage.py collectstatic && \
+    rm -f /var/www/ovis_web_svcs/sosgui/settings.py && \
+    mv /var/www/ovis_web_svcs/db.sqlite3 /custom/db.sqlite3 && \
+    ln -s /config/db.sqlite3 /var/www/ovis_web_svcs/db.sqlite3 && \
+    ln -s /config/settings.py /var/www/ovis_web_svcs/sosgui/settings.py && \
+    ln -s /config/httpd-wsgi.conf /etc/httpd/conf.d/wsgi.conf && \
+#    for file in passwd passwd- group ; do \
+#      mv /etc/$file /custom/ && \
+#      ln -s /config/etc/$file /etc/$file; \
+#    done && \
+    grep -v ^Listen /etc/httpd/conf/httpd.conf > /custom/httpd.conf && \
+    rm -f /etc/httpd/conf/httpd.conf && \
+    ln -s /config/httpd.conf /etc/httpd/conf/httpd.conf && \
+#    chown -R apache:apache /var/www/ovis_web_svcs /config /run /etc/httpd/logs && \
+#    chmod -R g+rw /var/www/ovis_web_svcs && \
     rm -f /etc/localtime && \
     chmod +x /custom/init.sh
 
--- custom/init.sh
+++ custom/init.sh
@@ -4,72 +4,40 @@ export PATH=/usr/local/sos/bin:$PATH
 export PYTHONPATH=/usr/local/sos/lib/python3.6/site-packages
 # Clear any old pid files
 rm -f /run/httpd/httpd.pid
-if [ ! -f /custom/initialized ] && [ "$rootless" == 1 ]; then
-	gpasswd -a apache root
-fi
 
-if [ ! -z "$apacheUID" ] && [ ! -f /custom/initialized ]; then
-	usermod -u $apacheUID apache
-	if [ ! -z "$apacheGID" ]; then
-		groupmod -g $apacheGID apache
-	fi
-	find /run /var /config/db.sqlite3 /custom -uid 48 |xargs chown apache:apache
-fi
-
-for file in /var/log/ovis_web_svcs/settings.log /var/log/ovis_web_svcs/sosgui.log ; do
-	if [ ! -f $file ]; then
-		touch $file
-		chown apache:apache $file
+#for file in /var/log/ovis_web_svcs/settings.log /var/log/ovis_web_svcs/sosgui.log ; do
+#	if [ ! -f $file ]; then
+#		touch $file
+#		chown apache:apache $file
+#	fi
+#done
+for file in httpd.conf httpd-wsgi.conf settings.py db.sqlite3 ; do
+	if [ ! -f /config/$file ]; then
+        cp /custom/$file /config/
 	fi
 done
-if [ ! -f /config/httpd-wsgi.conf ]; then
-        if [ -L /etc/httpd/conf.d/wsgi.conf ]; then
-                # reset
-                cp /custom/httpd-wsgi.conf /config
-        else
-		# init
-                cp /custom/httpd-wsgi.conf /config
-                ln -s /config/httpd-wsgi.conf /etc/httpd/conf.d/wsgi.conf
-	fi
-else
-        if [ ! -L /etc/httpd/conf.d/wsgi.conf ]; then
-                # init
-                ln -s /config/httpd-wsgi.conf /etc/httpd/conf.d/wsgi.conf
-        fi
-fi
 
-if [ ! -f /config/settings.py ]; then
-        # init or reset
-        if [ -L /var/www/ovis_web_svcs/sosgui/settings.py ]; then
-                # reset
-                cp /custom/settings.py /config
-        else
-                # init
-                cp /custom/settings.py /config
-                ln -s /config/settings.py /var/www/ovis_web_svcs/sosgui/settings.py
-        fi
-fi
+#if [ ! -d /config/etc/ ]; then
+#	mkdir -p /config/etc
+#fi
 
-if [ ! -L /var/www/ovis_web_svcs/db.sqlite3 ]; then
-	# init
-	python3 manage.py migrate && \
-	python3 manage.py migrate --run-syncdb && \
-	echo "from sosdb_auth.models import SosdbUser; SosdbUser.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python3 manage.py shell && \
-	python3 manage.py collectstatic
-	cp /var/www/ovis_web_svcs/db.sqlite3 /custom/db.sqlite3.firstrun
-	mv /var/www/ovis_web_svcs/db.sqlite3 /config/db.sqlite3
-	ln -s /config/db.sqlite3 /var/www/ovis_web_svcs/db.sqlite3
-	chown apache:apache /config/db.sqlite3 /config/settings.py
-else
-	# reset
-	if [ ! -f /config/db.sqlite3 ]; then
-		cp /custom/db.sqlite3.firstrun /config/db.sqlite3
-		chown apache:apache /config/db.sqlite3
-	fi	
-fi
-if [ ! -f /custom/initialized ]; then
-	touch /custom/initialized
-fi
+#for file in passwd passwd- group ; do
+#	if [ ! -f /config/etc/$file ]; then
+#        cp /custom/$file /config/etc/
+#	fi
+#done
+#		chown apache:apache /config/db.sqlite3
+#if [ ! -z "$apacheUID" ] && [ ! -f /config/etc/initialized ]; then
+#	usermod -u $apacheUID apache
+#fi
+#if [ ! -z "$apacheGID" ] && [ ! -f /config/etc/initialized ]; then
+#	groupmod -g $apacheGID apache
+#fi
+
+#if [ ! -f /config/etc/initialized ]; then
+#	find /run /var /config/db.sqlite3 /custom -uid 48 |xargs chown apache:apache
+#	touch /config/etc/initialized
+#fi
 
 /usr/sbin/httpd -D FOREGROUND
 
